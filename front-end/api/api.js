dd614d6b35c5b811e0746422e936e3b91f75d1b2
marcioHUNK <157060970+marcioHUNK@users.noreply.github.com>
2025-05-19 14:30:02 -0300

    Correção novamente na API para corrigir o nome e torna-lo final

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // const { NODE_ENV } = process.env;
-const URL = "https://para-teste.onrender.com/api";
+const URL = "https://spotikunay.onrender.com/api";
 // const URL = "https://deploy-jornada-full-stack.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
