7599bfec0673f44876ac1d83ac75caa23f657658
marcioHUNK <marciohenrique12@hotmail.com>
2025-05-19 13:34:26 -0300

    mudança na api da front-end para endereço do render - feito no trabalho

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // const { NODE_ENV } = process.env;
-const URL = "http://localhost:3001/api";
+const URL = "https://para-teste.onrender.com/api";
 // const URL = "https://deploy-jornada-full-stack.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
